c7065ee13c5fa327de6ea48326642359bcfb18c5
qiuchizhou <qiuchizhou@gmail.com>
2018-06-20 21:33:01 -0400

    adding phones to GitHub

---
+++ grade_calc.py
@@ -0,0 +1,11 @@
+grade = int(input("what's your grade?"))
+if 90 < grade <99:
+   print("A")
+elif 80 < grade < 89:
+    print("B")
+elif 70 < grade < 79:
+    print("C")
+elif 60 < grade < 70:
+    print("D")
+else:
+    print("F")
---
+++ phones.py
@@ -0,0 +1,3 @@
+phone = input("what's your number?\n")
+areacode = phone [0:3]
+print (areacode)
